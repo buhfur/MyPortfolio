61b21090f4c4b592922111f821106069c5a6a45d
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2021-02-22 20:05:39 -0500

    website displays bare html, doesnt load javascript

---
+++ ryanportfolio/src/Pages/home.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Header from '../Components/header'; 
+import Footer from '../Components/footer'; 
+class home extends Component {
+    render() {
+        return (
+		//This is the area where you paste your HTML codes
+		<body>
+
+		  
+		  
+			  <div class="probootstrap-section">
+			    <div class="container">
+
+			      <div class="row">
+				<div class="col-md-6 col-md-offset-3 mb40 text-center">
+				  <h2>Nature Photos</h2>
+				  <p>Posted  on September 3, 2017</p>
+				</div>
+				<div class="col-md-6">
+				  <p>Far far away, behind the word mountains, far from the countries <a href="#">Vokalia and Consonantia</a>, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small.</p>    
+				</div>
+				<div class="col-md-6">
+				  <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>    
+				</div>
+			      </div>
+			      <div class="row">
+				<div class="col-md-6">
+				  <p><a href="img/img_1.jpg" class="image-popup"><img src="img/img_1.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by uicookies.com"/></a></p>
+				  <p><a href="img/img_3.jpg" class="image-popup"><img src="img/img_3.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by uicookies.com"/></a></p>
+				  <p><a href="img/img_7.jpg" class="image-popup"><img src="img/img_7.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by uicookies.com"/></a></p>
+				</div>
+				<div class="col-md-6">
+				  <p><a href="img/img_2.jpg" class="image-popup"><img src="img/img_2.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by uicookies.com"/></a></p>
+				  <p><a href="img/img_4.jpg" class="image-popup"><img src="img/img_4.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by uicookies.com"/></a></p>
+				  <p><a href="img/img_6.jpg" class="image-popup"><img src="img/img_6.jpg" class="img-responsive" alt="Free HTML5 Bootstrap Template by uicookies.com"/></a></p>
+				</div>
+			      </div>
+			      
+			    </div>
+			  </div>51
+
+			 
+
+			  <div class="gototop js-top">
+			    <a href="#" class="js-gotop"><i class="icon-chevron-thin-up"></i></a>
+			  </div>
+			  
+
+			  <script src="js/scripts.min.js"></script>
+			  <script src="js/main.min.js"></script>
+			  <script src="js/custom.js"></script>
+		</body>
+)
+    }
+}
+export default home;
